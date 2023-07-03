be344515dfdafdc293f52388557f01deab856ec1
“TijesuRejoice” <tijesuadeleke94@gmail.com>
2023-07-03 04:08:56 -0700

    Task add two numbers without a function block

---
+++ 0-main.js
@@ -0,0 +1,12 @@
+/* This is a JavaScript file that contains little js code and snippets.
+
+
+<script>
+
+const num1 = 12;
+const num2 = 33;
+const numAdd = num1 + num2;
+
+console.log('The output is ' + numAdd + '. Thanks for trying!')
+
+</script>
